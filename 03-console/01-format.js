77277d173999c6ab086116e7d7cd3ba8063aefca
Sunhuizhe <674277449@qq.com>
2018-03-29 20:18:54 +0800

    控制台

---
+++ 03-console/01-format.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var user ={name:'Sun',
+           qq:'674277449'};
+
+console.log('name:%s',user.name);
+console.log('age:%d',21);
+console.log('JSON:%j',user);
+
+console.error('Error:something wrong!');
