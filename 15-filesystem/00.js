a3a0281938b51c97265cafbd3ec901c3698b20a9
Sunhuizhe <674277449@qq.com>
2018-03-29 20:22:50 +0800

    文件系统：同步/异步操作

---
+++ 15-filesystem/00.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+
---
+++ 15-filesystem/07-unsync-mkdir.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+
+var dir = process.argv[2];
+
+fs.mkdir(dir,function(err){
+  if(err){
+     console.log(err.message);
+     process.exit(1);
+  }
+})
+
+//fs.mkdirSync(dir);
